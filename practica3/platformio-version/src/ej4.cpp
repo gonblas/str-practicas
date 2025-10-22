09db12fe2468f4ec6de54a4730191324577788f4
Gonzalo Blasco <gonzagabino@gmail.com>
2025-10-22 19:50:13 -0300

    Practica 5 - Ejercicio 4 (fix)

--- practica3/platformio-version/src/ej4.cpp
+++ practica3/platformio-version/src/ej4.cpp
@@ -1,85 +1,106 @@
 #include <Arduino.h>
 #include <Arduino_FreeRTOS.h>
-
+#include <semphr.h>
 void TaskPrint(void *pvParameters);
-void TaskManager(void *pvParameters);
 
-TaskHandle_t hT1, hT2, hT3;
+SemaphoreHandle_t sems[3];
+SemaphoreHandle_t seqMutex;
+
+enum SequenceMode { SEQ_A = 0, SEQ_B = 1, SEQ_C = 2 };
+volatile SequenceMode sequenceMode;
+size_t stepIndex;
+
+static const uint8_t seqA[] = {0, 2, 1};
+static const uint8_t seqB[] = {1, 1, 2, 0};
+static const uint8_t seqC[] = {2, 2, 2, 0, 1};
+
+static const uint8_t *getSeq(SequenceMode m) {
+  switch (m) {
+    case SEQ_A: return seqA;
+    case SEQ_B: return seqB;
+    default:    return seqC;
+  }
+}
+
+static size_t getSeqLen(SequenceMode m) {
+  switch (m) {
+    case SEQ_A: return sizeof(seqA) / sizeof(seqA[0]);
+    case SEQ_B: return sizeof(seqB) / sizeof(seqB[0]);
+    default:    return sizeof(seqC) / sizeof(seqC[0]);
+  }
+}
+
+static SequenceMode nextSequence(SequenceMode m) {
+  return (SequenceMode)((m + 1) % 3);
+}
+
+const TickType_t d = pdMS_TO_TICKS(500);
 
 void setup()
 {
   Serial.begin(9600);
 
-  xTaskCreate(
-      TaskPrint,
-      "Tarea 1",
-      128,
-      (void *)"Tarea 1",
-      1,
-      &hT1);
+  for (int i = 0; i < 3; ++i) {
+    sems[i] = xSemaphoreCreateBinary();
+  }
 
-  xTaskCreate(
-      TaskPrint,
-      "Tarea 2",
-      128,
-      (void *)"Tarea 2",
-      1,
-      &hT2);
+  seqMutex = xSemaphoreCreateMutex();
 
-  xTaskCreate(
-      TaskPrint,
-      "Tarea 3",
-      128,
-      (void *)"Tarea 3",
-      1,
-      &hT3);
+  sequenceMode = SEQ_A;
+  stepIndex = 0;
 
-  xTaskCreate(
-      TaskManager,
-      "TaskManager",
-      128,
-      NULL,
-      1,
-      NULL);
+  Serial.println("-------Secuencia A-------");
+  xSemaphoreGive(sems[ seqA[0] ] );
+
+  xTaskCreate(TaskPrint, "Tarea 1", 128, (void *)(intptr_t)0, 1, NULL);
+  xTaskCreate(TaskPrint, "Tarea 2", 128, (void *)(intptr_t)1, 1, NULL);
+  xTaskCreate(TaskPrint, "Tarea 3", 128, (void *)(intptr_t)2, 1, NULL);
 
   vTaskStartScheduler();
 }
-
 
 void loop() {}
 
 void TaskPrint(void *pvParameters)
 {
-  char *taskName = (char *)pvParameters;
+  const int id = (int)(intptr_t)pvParameters;
+  const char *names[3] = {"Tarea 1", "Tarea 2", "Tarea 3"};
 
   for (;;)
   {
-    ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-    Serial.println(String(taskName));
-  }
-}
+    xSemaphoreTake(sems[id], portMAX_DELAY);
+    Serial.println(String("Tarea ") + String(id + 1));
 
-void TaskManager(void *pvParameters)
-{
-  const TickType_t d = pdMS_TO_TICKS(500);
-  for (;;)
-  { 
-    Serial.println("-------Secuencia A-------");
-    xTaskNotifyGive(hT1); vTaskDelay(d);
-    xTaskNotifyGive(hT3); vTaskDelay(d);
-    xTaskNotifyGive(hT2); vTaskDelay(d);
+    xSemaphoreTake(seqMutex, portMAX_DELAY);
+    SequenceMode curMode = sequenceMode;
+    const uint8_t *curSeq = getSeq(curMode);
+    size_t curLen = getSeqLen(curMode);
 
-    Serial.println("-------Secuencia B-------");
-    xTaskNotifyGive(hT2); vTaskDelay(d);
-    xTaskNotifyGive(hT2); vTaskDelay(d);
-    xTaskNotifyGive(hT3); vTaskDelay(d);
-    xTaskNotifyGive(hT1); vTaskDelay(d);
+    size_t nextIndex = (stepIndex + 1) % curLen;
+    int nextTask = -1;
 
-    Serial.println("-------Secuencia C-------");
-    xTaskNotifyGive(hT3); vTaskDelay(d);
-    xTaskNotifyGive(hT3); vTaskDelay(d);
-    xTaskNotifyGive(hT3); vTaskDelay(d);
-    xTaskNotifyGive(hT1); vTaskDelay(d);
-    xTaskNotifyGive(hT2); vTaskDelay(d);
+    if (nextIndex != 0) {
+      nextTask = curSeq[nextIndex];
+      stepIndex = nextIndex;
+    } else {
+      SequenceMode newMode = nextSequence(curMode);
+      const uint8_t *newSeq = getSeq(newMode);
+      nextTask = newSeq[0];
+      sequenceMode = newMode;
+      stepIndex = 0;
+      switch (newMode) {
+        case SEQ_A: Serial.println("-------Secuencia A-------"); break;
+        case SEQ_B: Serial.println("-------Secuencia B-------"); break;
+        case SEQ_C: Serial.println("-------Secuencia C-------"); break;
+      }
+    }
+
+    xSemaphoreGive(seqMutex);
+
+    vTaskDelay(d);
+
+    if (nextTask >= 0) {
+      xSemaphoreGive(sems[nextTask]);
+    }
   }
 }
